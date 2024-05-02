3343672039d0bbc1ff38be5255bda4917cb4d219
se1987 <s.tsukuda1987@gmail.com>
2024-05-03 06:47:17 +0900

    first commit

---
+++ proxy-server/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const app = express()
+
+const {createProxyMiddleware} = require("http-proxy-middleware")
+
+const rateLimit =
+    require("express-rate-limit")
+    require("dotenv").config()
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 10000,
+})
+
+app.use(limiter)
+
+app.get("/", (req, res) => {
+    res.send("This is my proxy server")
+})
+
+app.use("/world-weather-data", (req, res, next) => {
+    createProxyMiddleware({
+        target: process.env.BASE_API_URL_WORLD_WEATHER,
+        changeOrigin: true,
+        pathRewrite: {
+            [`^/world-weather-data`]: " ",
+        },
+    })(req, res, next)
+})
+
+const port = process.env.PORT || 5050
+
+app.listen(port, () => {
+    console.log(`Listening on localhost port ${port}`)
+})
+
+module.exports = app
